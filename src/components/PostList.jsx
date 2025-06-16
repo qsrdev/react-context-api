982dc12262d4dbe11616e161f092526fd328809c
qsrdev <matteo.albertini747@gmail.com>
2025-06-16 17:17:36 +0200

    feat: fixes

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,23 +1,23 @@
 // src/components/PostList.jsx
-import React from "react";
+import React, { useContext } from "react";
+import PostContext from "../context/PostContext";
 import { Link } from "react-router-dom";
-import { PostProvider } from "../context/PostContext";
 
 export default function PostList() {
-  const { posts } = PostProvider();
+  const { posts } = useContext(PostContext);
 
   return (
     <div className="row">
-      {posts.map((curPost) => (
-        <div key={curPost.id} className="col">
+      {posts.map((post) => (
+        <div key={post.id} className="col">
           <div className="card text-bg-dark mb-3">
-            <div className="card-header">{curPost.title}</div>
+            <div className="card-header">{post.title}</div>
             <div className="card-body">
-              <h5 className="card-title">{curPost.author}</h5>
-              <p className="card-text">{curPost.body}</p>
+              <h5 className="card-title">Autore sconosciuto</h5>
+              <p className="card-text">{post.body}</p>
             </div>
             <div className="card-footer">
-              <Link to={`/who/${curPost.id}`}>Ulteriori informazioni</Link>
+              <Link to={`/who/${post.id}`}>Ulteriori informazioni</Link>
             </div>
           </div>
         </div>
--- src/context/PostContext.jsx
+++ src/context/PostContext.jsx
@@ -3,8 +3,10 @@ import { createContext, useState } from "react";
 
 const PostContext = createContext();
 
-export const PostProvider = ({ children }) => {
+const PostProvider = ({ children }) => {
   const [posts, setPosts] = useState([]);
-
   return <PostContext.Provider value={{ posts, setPosts }}>{children}</PostContext.Provider>;
 };
+
+export { PostProvider };
+export default PostContext;
